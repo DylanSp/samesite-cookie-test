dd44d8815c30ec72f0b58c74c4e6ba40facd2ac9
Dylan Sprague <dylan.richard.sprague@gmail.com>
2020-10-24 19:14:25 -0400

    Set cookies from mainpage

--- mainpage/src/index.ts
+++ mainpage/src/index.ts
@@ -7,8 +7,12 @@ const port = 3000;
 const publicPath = path.join(__dirname, "..", "public");
 app.use(express.static(publicPath));
 
-app.get("/test", (req, res) => {
-  res.send("Hello world!!!");
+app.post("/setCookies", (req, res) => {
+  res.cookie("myCookie", "peanutButter", {
+    httpOnly: true,
+    sameSite: "strict"
+  });
+  res.sendStatus(204);
 });
 
 app.listen(port, () => {
